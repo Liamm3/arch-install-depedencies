e607b57c8cdc07cca6452476d4648b3cc97739c6
Liamm3 <liam.wohlfart0@gmail.com>
2022-01-30 17:26:18 +0100

    add some more repos

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash 
-yay -S ttf-font-awesome ttf-dejavu neovim neofetch pfetch spotify telegram-desktop teams librewolf-bin
+yay -S ttf-font-awesome ttf-dejavu neovim neofetch pfetch spotify telegram-desktop teams librewolf-bin xorg-server xorg-apps
